af9708efc6c75e0ace55e380d8ce8fca0a995ed7
kufte <kuftebg101@gmail.com>
2020-03-15 15:40:11 +0200

    moved addtocart back to orderslists

--- Pellio/Controllers/OrdersListsController.cs
+++ Pellio/Controllers/OrdersListsController.cs
@@ -30,7 +30,42 @@ namespace Pellio.Controllers
                 .Include(c => c.Products).FirstOrDefaultAsync(m => m.UserId == uid));
         }
 
-       
+        //POST: OrdersLists/AddToCart/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddToCart(int? id)
+        {
+            string uid = Request.Cookies["uuidc"];
+            var userorders = await _context.OrdersList
+            .FirstOrDefaultAsync(m => m.UserId == uid);
+            if (userorders == null)
+            {
+                userorders = new OrdersList
+                {
+                    Total = 0,
+                    UserId = uid
+                };
+                _context.OrdersList.Add(userorders);
+            }
+            if (userorders.Products == null)
+            {
+                userorders.Products = new List<Products>();
+            }
+            var product = _context.Products.Find(id);
+            var newproduct = new Products
+            {
+                ProductName = product.ProductName,
+                Price = product.Price,
+                ImageUrl = product.ImageUrl,
+                Ingredients = "dont show"
+            };
+
+            userorders.Products.Add(newproduct);
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
         //sendmail
         [HttpPost]
         public async Task<IActionResult> SendMail(string rec, string mes)
