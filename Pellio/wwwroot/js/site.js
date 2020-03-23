24eb8bef5bffe5a70e01aa719f43a192f4f56a6e
HristovCodes <>
2020-03-23 19:42:52 +0200

    Changed navigation completely

--- Pellio/wwwroot/js/site.js
+++ Pellio/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const burgeropen = document.getElementById("burgeropen");
+const burgerclose = document.getElementById("burgerclose");
+const menu = document.getElementById("menu");
+
+burgeropen.addEventListener("click", () => {
+    menu.classList.add("menu")
+})
+
+burgerclose.addEventListener("click", () => {
+    menu.classList.remove("menu");
+})
