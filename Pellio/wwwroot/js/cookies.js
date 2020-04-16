b543053d089a742f48b531662f693d172e5824df
HristovCodes <ivailohristov1990@gmail.com>
2020-04-16 10:53:02 +0300

    Comments don't stack on one another anymore

--- Pellio/wwwroot/js/cookies.js
+++ Pellio/wwwroot/js/cookies.js
@@ -1,12 +1,12 @@
 ﻿const cookieswindow = document.getElementById("cookieswindow");
 const acceptcookies = document.getElementById("cookiesagree");
 
-if (document.cookie.includes("accepted=true")) {
-    cookieswindow.className = "hide";
-} else {
-    acceptcookies.className = "window";
+if (!document.cookie.includes("accepted=true")) {
+    cookieswindow.className = "window";
     acceptcookies.addEventListener("click", () => {
         cookieswindow.className = "hide";
         document.cookie = "accepted=true";
     });
+} else {
+    cookieswindow.className = "hide";
 }
