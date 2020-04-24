a3e722b60f777c5a89e4f061155d3cb5f5fecb0e
HristovCodes <ivailohristov1990@gmail.com>
2020-04-24 15:32:03 +0300

    Deleted some unnecesary code

--- Pellio/Controllers/ProductsController.cs
+++ Pellio/Controllers/ProductsController.cs
@@ -172,6 +172,7 @@ namespace Pellio.Controllers
         }
 
         //POST: Products/AddComment/5
+        //Adds a comment to the database that is associated with a certain product.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> AddComment(int id, [Bind("Name,Comment,Score")] Comments comments)
@@ -188,147 +189,13 @@ namespace Pellio.Controllers
             return View(comments);
         }
 
-        // GET: Products/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var products = await _context.Products
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (products == null)
-            {
-                return NotFound();
-            }
-
-            return View(products);
-        }
-
-        // GET: Products/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Products/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ProductName,Ingredients,Price,ImageUrl")] Products products)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(products);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(products);
-        }
-
-        // GET: Products/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var products = await _context.Products.FindAsync(id);
-            if (products == null)
-            {
-                return NotFound();
-            }
-            return View(products);
-        }
-
-        // POST: Products/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,ProductName,Ingredients,Price,ImageUrl")] Products products)
-        {
-            if (id != products.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(products);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ProductsExists(products.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(products);
-        }
-
-        // GET: Products/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var products = await _context.Products
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (products == null)
-            {
-                return NotFound();
-            }
-
-            return View(products);
-        }
-
-        // POST: Products/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var products = await _context.Products.FindAsync(id);
-            _context.Products.Remove(products);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        //POST : Products/DeleteAll
-        [HttpPost, ActionName("DeleteAll")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteAll()
-        {
-            foreach (Products item in _context.Products)
-            {
-                _context.Products.Remove(item);
-            }
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
         //POST : Products/FillDB
+        //Reads a txt with testing data. For testing pursposes on multiple devices.
         [HttpPost, ActionName("FillDB")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> FillDB()
         {
-            string line;
-
-            // Read the file and display it line by line.  
+            string line; 
             string file_name = "Danni.txt";
             string path = Path.Combine(Environment.CurrentDirectory, file_name);
             System.IO.StreamReader file =
@@ -343,19 +210,13 @@ namespace Pellio.Controllers
                 tobeadded.ImageUrl = values[3];
                 tobeadded.Tag = values[4];
                 _context.Products.Add(tobeadded);
-                //_context.Products.Add(new Products
-                //{
-                //    ProductName = values[0],
-                //    Ingredients = values[1].Trim('\"'),
-                //    Price = Convert.ToDecimal(values[2].Trim('\"')), //Convert.ToDecimal(values[2].Trim('\"'))
-                //    ImageUrl = values[3]
-                //}); ;
             }
             file.Close();
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
+        //Checks if a product exists. Accepts ID as an argument.
         private bool ProductsExists(int id)
         {
             return _context.Products.Any(e => e.Id == id);
