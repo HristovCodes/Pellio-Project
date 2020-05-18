b2dfda39d7912ca691a1f8c5a1a12178b71df231
HristovCodes <ivailohristov1990@gmail.com>
2020-05-18 11:36:53 +0300

    Added geolocation finder

--- Pellio/wwwroot/js/formvalidation.js
+++ Pellio/wwwroot/js/formvalidation.js
@@ -4,3 +4,28 @@
 const emailRegExp = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$";
 
 email.setAttribute("pattern", emailRegExp);
+
+getLoc();
+
+function getLoc() {
+    let status = document.getElementById("status");
+
+    function success(position) {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+
+        status.innerHTML = latitude + " | " + longitude;
+    }
+
+    function error() {
+        status.textContent = 'Unable to retrieve your location';
+    }
+
+    if (!navigator.geolocation) {
+        alert('Geolocation is not supported by your browser');
+    } else {
+        status.innerHTML = 'Locating…';
+        navigator.geolocation.getCurrentPosition(success, error);
+    }
+
+}
